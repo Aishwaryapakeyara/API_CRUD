d3fc020f69e1102641fb240a334964823afbcab2
Aishwarya Pakeyara <apakeyara@gmail.com>
2022-07-18 15:32:18 +0530

    Config files

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+  post "/create", to: "users#create"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
 
 end
